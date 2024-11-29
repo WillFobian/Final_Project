995ddf265c9ad38ca2b0e59136fb219fd3a4ebf5
William Fobian <willfobian@gmail.com>
2024-11-29 12:26:37 -0600

    added position and size and rotation random generators

--- Procedural_model.py
+++ Procedural_model.py
@@ -1,3 +1,11 @@
 import maya.cmds as cmds
 import random
 import math
+
+def create_random_cube():
+    x_pos = random.uniform(-10, 10)
+    y_pos = random. uniform(0, 5)
+    z_pos = random.uniform(-10, 10)
+    size = random.uniform(1, 5)
+    rotation = [random.uniform(0, 360) for _ in range(3)]
+    
