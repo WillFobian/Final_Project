af38ae7428816bb8e3f110b19e3775659809c5af
William Fobian <willfobian@gmail.com>
2024-11-29 13:35:34 -0600

    added cube and commands to execute the random generator

--- Procedural_model.py
+++ Procedural_model.py
@@ -8,4 +8,10 @@ def create_random_cube():
     z_pos = random.uniform(-10, 10)
     size = random.uniform(1, 5)
     rotation = [random.uniform(0, 360) for _ in range(3)]
-    
+    cube = cmds.polyCube(w=size, h=size, d=size)[0]
+    cmds.move(x_pos, y_pos, z_pos, cube)
+    cmds.rotate(rotation[0], rotation[1], rotation[2], cube)
+    apply_random_color(cube)
+
+    return cube
+
