a3f5726a2b698fc9f11e4f15cd9984acf33e01cc
William Fobian <willfobian@gmail.com>
2024-12-02 13:41:09 -0600

    finished sphere function and added apply random color

--- Procedural_model.py
+++ Procedural_model.py
@@ -19,3 +19,17 @@ def create_random_sphere():
     x_pos = random.uniform(-10, 10)
     y_pos = random. uniform(0, 5)
     z_pos = random.uniform(-10, 10)
+    radius = random.uniform(1,3)
+    rotation = [random.uniform(0, 360) for _ in range(3)]
+    sphere = cmds.polySphere(r=radius)[0]
+    cmds.move(x_pos, y_pos, z_pos, sphere)
+    cmds.rotate(rotation[0], rotation[1], rotation[2], sphere)
+
+
+
+
+def apply_random_color(obj):
+    shader = cmds.shadingNode('lambert', asShader=True)
+    shading_group = cmds.setAttr(shader + ".color", random.random(), random.random(), random.random(), type="double3")
+    cmds.select(obj)
+    cmds.hyperShade(assign=shader)
